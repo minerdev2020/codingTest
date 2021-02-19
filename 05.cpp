1a1a2b960da4bc5edc15b2653bc945d8d6151f99
HevilH <han051597@naver.com>
2021-02-19 20:56:13 +0900

    H-index    /정렬

---
+++ 05.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(),greater<>());
+    for(int i = 0; i < citations.size(); i++){
+        if(i + 1 > citations[i]){
+            answer = i;
+            break;
+        }
+        if(i == i < citations.size() - 1)
+            answer = citations.size();
+    }
+    return answer;
+}
