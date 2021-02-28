4d316bc2ecafeb53f577da39f89f27db20ac9e79
HevilH <han051597@naver.com>
2021-02-28 21:07:52 +0900

    징검다리 /dp

---
+++ 14.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(int N, int number) {
+    int answer = 0;
+    
+    vector<int> dp[8+1];
+    int base = 0;
+    for(int i = 1; i < 9; i++){
+        base = base * 10 + N;
+        dp[i].push_back(base);
+    }
+
+    for(int i = 1; i < 9; i++){
+        for(int j = 1; j < i; j++){
+                 for(auto& x:dp[j]){
+                    for(auto& y:dp[i-j]){
+                        dp[i].push_back(x + y);
+                        dp[i].push_back(x - y);
+                        dp[i].push_back(x * y);
+                        if(y != 0)    
+                            dp[i].push_back(x / y);
+                    }
+                 }
+        }
+        if(count(dp[i].begin(),dp[i].end(),number) > 0){
+            answer = i;
+            break;
+        }
+    }
+    
+    
+    return answer==0?-1:answer;
+}
