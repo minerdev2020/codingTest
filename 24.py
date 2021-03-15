6fe8271ee83d55197c4cb6a90adf2e33c91ccb8f
HevilH <han051597@naver.com>
2021-03-15 21:29:09 +0900

    설탕배달 /dp, greedy

--- 24.py
+++ 24.py
@@ -1,3 +1,25 @@
+#dp
+def solution(num):
+    dp = [0 for _ in range(20000)]
+    dp[3] = dp[5] = 1
+    for i in range(6, num + 1):
+        if dp[i-3]:
+            dp[i] = dp[i-3] + 1
+        if(dp[i-5]):
+            if(dp[i]):
+                dp[i] =  min(dp[i]+1 , dp[i - 5] + 1)
+            else:
+                dp[i] = dp[i-5] + 1
+    if dp[num] == 0:
+        return -1
+    else:
+        return dp[num]
+
+
+num = int(input())
+print(solution(num))
+
+#greedy
 N = int(input()) 
 three = 0
 five = N // 5
